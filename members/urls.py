d69bf1611724f737b50621cec6a5a084a223d104
jQrgitQ <158132525+jQrgitQ@users.noreply.github.com>
2024-12-01 08:54:01 +0100

    avance en el curso

--- members/urls.py
+++ members/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('testing/', views.testing, name='testing'),
     path('members/', views.members, name='members'),
     path('members/details/<int:id>', views.details, name='details'),
+
 ]
 
 
