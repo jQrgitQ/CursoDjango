3a09fa1e7284b704761b57c827cb60914cde3d3d
jQrgitQ <158132525+jQrgitQ@users.noreply.github.com>
2024-12-01 13:54:49 +0100

    antes de que se rompa

--- djangoProjectPrueba1/settings.py
+++ djangoProjectPrueba1/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     ##"members.apps.MembersConfig"
     'members'
+
 ]
 
 MIDDLEWARE = [
@@ -118,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- members/models.py
+++ members/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Member(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
-  phone = models.IntegerField(null=True)
-  joined_date = models.DateField(null=True)
+  phone = models.IntegerField()
+  joined_date = models.DateField()
+
+  def __str__(self):
+    return f"{self.firstname} {self.lastname}"
