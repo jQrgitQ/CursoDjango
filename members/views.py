0f509e0b6396a9a8fd9ff8490fd7352fa7ad2c01
jQrgitQ <158132525+jQrgitQ@users.noreply.github.com>
2024-12-01 08:54:01 +0100

    avance en el curso

--- members/views.py
+++ members/views.py
@@ -29,3 +29,10 @@ def details(request, id):
 def main(request):
   template = loader.get_template('main.html')
   return HttpResponse(template.render())
+
+def testing(request):
+  template = loader.get_template('testing.html')
+  context = {
+    'fruits': ['apple','orange','pear'],
+  }
+  return HttpResponse(template.render(context, request))
