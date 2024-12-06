9c648f53f36fea97d2de1a07f88722619057e244
jQrgitQ <158132525+jQrgitQ@users.noreply.github.com>
2024-12-06 22:53:25 +0100

    2

--- members/admin.py
+++ members/admin.py
@@ -1,6 +1,10 @@
 #import Member
+from cProfile import Profile
+
 from django.contrib import admin
 from members.models import Member
+#from members.models.teacher.models import Teacher
 
 # Register your models here.
 admin.site.register(Member)
+#admin.site.register(Teacher)
